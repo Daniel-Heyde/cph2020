82e32bb7cdbc135285212aead86ecbfe53b7f96d
Kevin Krein <kevin.krein98@gmail.com>
2020-02-08 20:40:10 -0800

    Added start to question asking

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import logo from './assets/logo.svg';
+import MenuBar from './components/MenuBar'
+import HorizontalLabelPositionBelowStepper from './components/Questions'
 import './css/App.css';
 
 class App extends Component {
   render() {
       return (
-          <div className="App">
-              <h1>Yo</h1>
-          </div>
+        <div>
+          <MenuBar />
+          <HorizontalLabelPositionBelowStepper />
+        </div>
       )
   }
 }
